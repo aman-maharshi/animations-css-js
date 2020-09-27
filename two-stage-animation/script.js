12e13b0bced826265789b9b036e96684d7fdf243
aman-maharshi <amanm4harshi@gmail.com>
2020-09-27 19:59:01 +0530

    animation complete

--- two-stage-animation/script.js
+++ two-stage-animation/script.js
@@ -1,6 +1,19 @@
 const box = document.querySelector(".box")
+const topText = document.querySelector(".top-text")
+const bottomText = document.querySelector(".bottom-text")
 
 box.addEventListener("click", () => {
-    console.log("click")
-    box.classList.toggle("box-enlarge")
+    if (!box.classList.contains("box-enlarge")) {
+        box.classList.add("box-enlarge")
+        setTimeout(() => {
+            topText.classList.add("pull-right")
+            bottomText.classList.add("pull-left")
+        }, 500)
+    } else {
+        topText.classList.remove("pull-right")
+        bottomText.classList.remove("pull-left")
+        setTimeout(() => {
+            box.classList.remove("box-enlarge")
+        }, 500)
+    }
 })
