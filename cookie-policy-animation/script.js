9492493462daf8e5fa7400c89b6b6e2588fc3c81
aman-maharshi <amanm4harshi@gmail.com>
2023-05-16 15:03:45 +0530

    card - image and text onload animations

---
+++ cookie-policy-animation/script.js
@@ -0,0 +1,30 @@
+const timeline = gsap.timeline({
+  defaults: {
+    duration: 0.75,
+    ease:Power1.easeOut
+  }
+})
+
+timeline.fromTo(
+  ".cookie-container",
+  {scale: 0},
+  {scale: 1}
+)
+timeline.fromTo(
+  ".cookie",
+  {opacity: 0, x: -50, rotation: "-45deg"},
+  {opacity: 1, x: 0, rotation: "0deg"}
+)
+timeline.fromTo(
+  ".text",
+  {opacity: 0, x: 50},
+  {opacity: 1, x: 0},
+  "<" // to sync .text animation to the above .cookie one
+)
+
+document.querySelector('button').addEventListener("click", () => {
+  timeline.to(
+    ".cookie-container",
+    {opacity: 0, y: 100, duration: 0.5, ease:Power1.easeOut}
+  )
+})
