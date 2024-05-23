8cd8fe5e98dfff097a59e8639ceb1dcc0c9b5d32
aman-maharshi <amanm4harshi@gmail.com>
2024-05-23 12:21:29 +0530

    desktop ui updates

--- parallax-cards/src/Card.js
+++ parallax-cards/src/Card.js
@@ -2,11 +2,24 @@ import React from 'react'
 
 function Card({ item }) {
 
-  const {title, description, src, link, color} = item
+  const { title, description, src, link, color } = item
   return (
     <div className='card-container'>
-      <div className="card" style={{background: color}}>
-        
+      <div className="card" style={{ background: color }}>
+        <div className="card__title">{title}</div>
+        <div className="card__row">
+          <div className="card__left">
+            <div className="card__description">{description}</div>
+            <div className="card__link">
+              <a href={link} target="_blank" rel="noreferrer">Go to website</a>
+            </div>
+          </div>
+          <div className="card__right">
+            <div className="card__image">
+              <img src={src} alt={title} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
